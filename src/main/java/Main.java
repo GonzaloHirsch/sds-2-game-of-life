2218b3cae887ab8b5c7609f8b27ae4344b9e9065
Gonza Hirsch <hirschgonzalo@yahoo.com.ar>
2020-08-23 14:21:49 -0300

    Refactor and adding file writing

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,11 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.Collection;
 import java.util.List;
 
 public class Main {
@@ -13,18 +21,33 @@ public class Main {
         List<int[]> pointsToWrite;
 
         for (int i = 1; i < totalSteps; i++){
+            // Simulating the step
             pointsToWrite = gol.simulateStep();
 
-            /*
-            * [
-            *  [x, y, z],
-            *  [z, y, z]
-            *
-            *
-            * ]
-            * */
+            // Writing results to file
+            GenerateOutputFile(pointsToWrite);
+        }
+    }
 
-            // WRITE AL ARCHIVO
+    private static void GenerateOutputFile(List<int[]> cells) {
+        try {
+            // StringBuilder to minimize file writes
+            StringBuilder sb = new StringBuilder();
+
+            // Creating the output for the file
+            cells.forEach(cell -> {
+                if (cell.length == 3){
+                    sb.append(String.format("%d %d %d\n", cell[0], cell[1], cell[2]));
+                } else {
+                    sb.append(String.format("%d %d\n", cell[0], cell[1]));
+                }
+            });
+
+            Files.write(Paths.get(OptionsParser.dynamicFile), sb.toString().getBytes(), StandardOpenOption.APPEND);
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        } catch (IOException e) {
+            System.out.println("Error writing to the output file");
         }
     }
 }
